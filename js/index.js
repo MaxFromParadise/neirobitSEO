a58eaafeb3efb058be20eef05b2993178d3119c6
MaxFromParadise <112644100+MaxFromParadise@users.noreply.github.com>
2024-11-16 07:22:25 +0000

    full upload

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const burger = document.querySelector('.burger');
 	const menu = document.querySelector('.nav__list');
+	const menuClose = document.querySelector('.nav__close');
 
 	burger.addEventListener('click', () => {
 		burger.classList.toggle('active');
 		menu.classList.toggle('active');
+	});
+	menuClose.addEventListener('click', () => {
+		burger.classList.remove('active');
+		menu.classList.remove('active');
+	});
+	const swiper = new Swiper('.portfolio__slider', {
+		slidesPerView: 'auto',
+		centeredSlides: true,
+		spaceBetween: 30,
+		loop: false,
+		navigation: {
+			nextEl: '.portfolio__button--next',
+			prevEl: '.portfolio__button--prev',
+		},
+		pagination: {
+			el: '.portfolio__pagination',
+			clickable: true,
+		},
 	});
 });
